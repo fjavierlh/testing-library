ab66d190b2460fa60d6edd4cc4a116d106d9721c
Javier López <fjavierlh@gmail.com>
2024-01-27 19:54:02 +0100

    feat: create test util

--- src/stats.test.ts
+++ src/stats.test.ts
@@ -1,12 +1,14 @@
-import { expect } from "./test-lib";
+import { expect, test } from "./test-lib";
 import { average, sum } from "./stats";
 
-let result = sum([1, 2, 3]);
-let expected = 6;
-expect(expected).toBe(result);
+test("should calculates the sum of all elements of the array", () => {
+  const result = sum([1, 2, 3]);
+  const expected = 6;
+  expect(expected).toBe(result);
+});
 
-result = average([1, 2, 3]);
-expected = 2;
-expect(expected).toBe(result);
-
-
+test("should calculates the average of all elements of the array", () => {
+  const result = average([1, 2, 3]);
+  const expected = 2;
+  expect(expected).toBe(result);
+});
