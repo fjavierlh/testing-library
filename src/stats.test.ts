1690470e71d3401d4b80d31cbd248aba07542230
Javier López <fjavierlh@gmail.com>
2024-01-27 17:05:14 +0100

    feat: create sum and average utils and add primitive tests

---
+++ src/stats.test.ts
@@ -0,0 +1,19 @@
+import { average, sum } from "./stats";
+
+let result = sum([1, 2, 3]);
+let expected = 6;
+
+if (result === expected) {
+  console.log("✅");
+} else {
+  throw new Error(`❌ ${result} is not equal to ${expected}`);
+}
+
+result = average([1, 2, 3]);
+expected = 2;
+
+if (result === expected) {
+  console.log("✅");
+} else {
+  throw new Error(`❌ ${result} is not equal to ${expected}`);
+}
