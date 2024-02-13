cb04f02cb070845e6ef78d25a5c9d6b8ecc5062f
Javier López <fjavierlh@gmail.com>
2024-02-13 07:31:48 +0100

    chore: move async tests to own file

---
+++ src/stats-async.test.ts
@@ -0,0 +1,15 @@
+import * as asyncStats from "./stats-async";
+
+describe("Async stats should", () => {
+  it("calculates the async um of all elements of the array", async () => {
+    const result = await asyncStats.sum([1, 2, 3]);
+    const expected = 6;
+    expect(expected).toBe(result);
+  });
+
+  it("calculates the async average of all elements of the array", async () => {
+    const result = await asyncStats.average([1, 2, 3]);
+    const expected = 2;
+    expect(expected).toBe(result);
+  });
+});
--- src/stats.test.ts
+++ src/stats.test.ts
@@ -1,5 +1,4 @@
 import * as stats from "./stats";
-import * as asyncStats from "./stats-async";
 
 describe("Stats should", () => {
   it("calculates the sum of all elements of the array", () => {
@@ -10,20 +9,6 @@ describe("Stats should", () => {
 
   it("calculates the average of all elements of the array", () => {
     const result = stats.average([1, 2, 3]);
-    const expected = 2;
-    expect(expected).toBe(result);
-  });
-});
-
-describe("Async stats should", () => {
-  it("calculates the async um of all elements of the array", async () => {
-    const result = await asyncStats.sum([1, 2, 3]);
-    const expected = 6;
-    expect(expected).toBe(result);
-  });
-
-  it("calculates the async average of all elements of the array", async () => {
-    const result = await asyncStats.average([1, 2, 3]);
     const expected = 2;
     expect(expected).toBe(result);
   });
