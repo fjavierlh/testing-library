1095e4772373e7190dae1882a69d60b5e0ee6589
Javier López <fjavierlh@gmail.com>
2024-02-14 07:51:55 +0100

    feat: made fizzbuzz kata using tdd

---
+++ src/fizz-buzz.test.ts
@@ -0,0 +1,59 @@
+import { fizzBuzz } from "./fizz-buzz";
+
+describe("fizzBuzz", () => {
+  test("should return '1' if received param is 1", () => {
+    const result = fizzBuzz(1);
+    const expected = "1";
+
+    expect(result).toBe(expected);
+  });
+
+  test("should return 'fizz' if received param is 3", () => {
+    const result = fizzBuzz(3);
+    const expected = "fizz";
+
+    expect(result).toBe(expected);
+  });
+
+  test("should return 'buzz' if received param is 5", () => {
+    const result = fizzBuzz(5);
+    const expected = "buzz";
+
+    expect(result).toBe(expected);
+  });
+
+  test("should return 'fizzbuzz' if received param is 15", () => {
+    const result = fizzBuzz(15);
+    const expected = "fizzbuzz";
+
+    expect(result).toBe(expected);
+  });
+
+  test("should return 'fizz' if received param divisible by 3", () => {
+    const result = fizzBuzz(12);
+    const expected = "fizz";
+
+    expect(result).toBe(expected);
+  });
+
+  test("should return 'buzz' if received param divisible by 5", () => {
+    const result = fizzBuzz(10);
+    const expected = "buzz";
+
+    expect(result).toBe(expected);
+  });
+
+  test("should return 'fizzbuzz' if received param divisible by 15", () => {
+    const result = fizzBuzz(30);
+    const expected = "fizzbuzz";
+
+    expect(result).toBe(expected);
+  });
+
+  test("should return same stringified number if is not divisible by 3, 5, or 15", () => {
+    const result = fizzBuzz(4);
+    const expected = "4";
+
+    expect(result).toBe(expected);
+  });
+});
---
+++ src/fizz-buzz.ts
@@ -0,0 +1,11 @@
+export function fizzBuzz(number: number): string {
+  if (number % 15 === 0) {
+    return "fizzbuzz";
+  } else if (number % 3 === 0) {
+    return "fizz";
+  } else if (number % 5 === 0) {
+    return "buzz";
+  } else {
+    return number.toString();
+  }
+}
