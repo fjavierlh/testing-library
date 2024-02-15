1f7751b30d00a36326df0fb1b251ccc09472449a
Javier López <fjavierlh@gmail.com>
2024-02-15 07:37:10 +0100

    refactor: create capitalize function

--- src/camel-case-converter.ts
+++ src/camel-case-converter.ts
@@ -1,6 +1,8 @@
 export function camelCaseConverter(text: string): string {
+  function capitalize(word: string): string {
+    return word.charAt(0).toUpperCase() + word.substring(1);
+  }
+
   const words = text.split(/\s|_{1,}|-{1,}/);
-  return words
-    .map((word) => word.charAt(0).toUpperCase() + word.substring(1))
-    .join("");
+  return words.map(capitalize).join("");
 }
