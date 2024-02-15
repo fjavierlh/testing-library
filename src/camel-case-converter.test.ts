09aafb1bd68a9174be432cb90ad1bcf4c2af9408
Javier López <fjavierlh@gmail.com>
2024-02-15 07:53:27 +0100

    refactor: modify redundant test names

--- src/camel-case-converter.test.ts
+++ src/camel-case-converter.test.ts
@@ -1,35 +1,35 @@
 import { camelCaseConverter } from "./camel-case-converter";
 
 describe("Camel case converter", () => {
-  test("should allow empty text", () => {
+  test("allow empty text", () => {
     expect(camelCaseConverter("")).toBe("");
   });
 
-  test("should allow capitalized word", () => {
+  test("allow capitalized word", () => {
     expect(camelCaseConverter("Foo")).toBe("Foo");
   });
 
-  test("should join capitalized words separated by spaces", () => {
+  test("join capitalized words separated by spaces", () => {
     expect(camelCaseConverter("Foo Bar")).toBe("FooBar");
   });
 
-  test("should join capitalized words separated by spaces", () => {
+  test("join capitalized words separated by spaces", () => {
     expect(camelCaseConverter("Foo Bar")).toBe("FooBar");
   });
 
-  test("should join capitalized words separated by hyphens", () => {
+  test("join capitalized words separated by hyphens", () => {
     expect(camelCaseConverter("Foo__Bar--Foo")).toBe("FooBarFoo");
   });
 
-  test("should join capitalized words separated by hyphens and spaces", () => {
+  test("join capitalized words separated by hyphens and spaces", () => {
     expect(camelCaseConverter("Foo__Bar--Foo Bar")).toBe("FooBarFooBar");
   });
 
-  test("should convert one word to capitalize", () => {
+  test("convert one word to capitalize", () => {
     expect(camelCaseConverter("foo")).toBe("Foo");
   });
 
-  test("should convert multiple words to capitalize", () => {
+  test("convert multiple words to capitalize", () => {
     expect(camelCaseConverter("foo bar foo")).toBe("FooBarFoo");
   });
 });
