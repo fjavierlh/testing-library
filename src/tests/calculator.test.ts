dc24a912c5e8c72c9dfd2925b6bdde0773fbe7db
Javier López <fjavierlh@gmail.com>
2024-02-23 16:05:28 +0100

    feat: add jest mock example

--- src/tests/calculator.test.ts
+++ src/tests/calculator.test.ts
@@ -1,19 +1,18 @@
 import * as calculator from "../calculator";
 import * as arithmetic from "../arithmetic";
 
-describe("calculator should", () => {
-  (arithmetic as any).add = jest.fn(arithmetic.add);
-  (arithmetic as any).subtract = jest.fn(arithmetic.subtract);
+jest.mock("../arithmetic");
 
+describe("calculator should", () => {
   test("calls arithmetic add", () => {
     const result = calculator.doAdd(1, 2);
     expect(arithmetic.add).toHaveBeenLastCalledWith(1, 2);
-    expect(result).toBe(3)
+    // expect(result).toBe(3);
   });
 
   test("calls arithmetic subtract", () => {
     const result = calculator.doSubtract(1, 2);
     expect(arithmetic.subtract).toHaveBeenLastCalledWith(1, 2);
-    expect(result).toBe(-1)
+    // expect(result).toBe(-1);
   });
 });
