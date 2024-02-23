5e394f0f1fd49891ab8de794533a124102955684
Javier López <fjavierlh@gmail.com>
2024-02-23 16:08:52 +0100

    feat: add spy example

--- src/tests/calculator.test.ts
+++ src/tests/calculator.test.ts
@@ -1,18 +1,18 @@
 import * as calculator from "../calculator";
 import * as arithmetic from "../arithmetic";
 
-jest.mock("../arithmetic");
-
 describe("calculator should", () => {
   test("calls arithmetic add", () => {
+    const mockedAdd = jest.spyOn(arithmetic, "add");
     const result = calculator.doAdd(1, 2);
-    expect(arithmetic.add).toHaveBeenLastCalledWith(1, 2);
-    // expect(result).toBe(3);
+    expect(mockedAdd).toHaveBeenLastCalledWith(1, 2);
+    expect(result).toBe(3);
   });
 
   test("calls arithmetic subtract", () => {
+    const mockedSubtract = jest.spyOn(arithmetic, "subtract");
     const result = calculator.doSubtract(1, 2);
-    expect(arithmetic.subtract).toHaveBeenLastCalledWith(1, 2);
-    // expect(result).toBe(-1);
+    expect(mockedSubtract).toHaveBeenLastCalledWith(1, 2);
+    expect(result).toBe(-1);
   });
 });
