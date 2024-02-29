68d06ba711efb83bb4698d0d482d9dc8912c04f9
Javier López <fjavierlh@gmail.com>
2024-02-29 07:46:01 +0100

    test: add new minimun character lenght ususe case

--- tests/password-validator.test.ts
+++ tests/password-validator.test.ts
@@ -4,5 +4,6 @@ describe("The password validator", () => {
   test("validate that a password has the minimum characters", () => {
     const validator = new PasswordValidator();
     expect(validator.execute("123456")).toBeTruthy();
+    expect(validator.execute("1234")).toBeFalsy();
   });
 });
