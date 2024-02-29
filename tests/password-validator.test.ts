ba88b3f0bedd4e063fddfc29ecb49a313fbae2e0
Javier López <fjavierlh@gmail.com>
2024-02-29 07:38:48 +0100

    test(red): create first case of password validator

---
+++ tests/password-validator.test.ts
@@ -0,0 +1,6 @@
+describe("The password validator", () => {
+  test("validate that a password has the minimum characters", () => {
+    const validator = new PasswordValidator();
+    expect(validator.execute("123456")).toBeTruthy();
+  });
+});
