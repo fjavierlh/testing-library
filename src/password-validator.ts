a6e7a6c090ec29585167cbd74247c46dd683eeb2
Javier López <fjavierlh@gmail.com>
2024-03-01 07:15:37 +0100

    fix: avoid return null

--- src/password-validator.ts
+++ src/password-validator.ts
@@ -2,6 +2,9 @@ export class PasswordValidator {
   public execute(password: string): boolean {
     const minimumPasswordLength = 6;
     const containsDigit = password.match(/\d/);
-    return containsDigit && password.length >= minimumPasswordLength;
+
+    return [containsDigit, password.length >= minimumPasswordLength].every(
+      Boolean
+    );
   }
 }
