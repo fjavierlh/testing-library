f50f1cf40a511408360f2eaf419d22ccd504c580
Javier López <fjavierlh@gmail.com>
2024-03-01 16:29:45 +0100

    refactor: create util function to isolate not use case methods

--- tests/password-validator.test.ts
+++ tests/password-validator.test.ts
@@ -1,30 +1,58 @@
 import { PasswordValidator } from "../src/password-validator";
 
 describe("The password validator", () => {
-  const validPassword = "1_Abcd";
-  const invalidPassword = "abcde";
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
 
-  it.each`
-    password           | expected
-    ${validPassword}   | ${true}
-    ${invalidPassword} | ${false}
-  `(
-    "checks that a password has the allowed minimum characters",
-    ({ password, expected }) => {
-      const validator = new PasswordValidator();
-      expect(validator.execute(password)).toBe(expected);
-    }
-  );
+  it("checks that a password has the allowed minimum characters", () => {
+    bypassIrrelevantPrivateMethodsForUseCase("hasMinimumLength");
+    const minimumLength = 6;
+    const incorrectLength = 5;
+    const validPassword = "*".repeat(minimumLength);
+    const invalidPassword = "*".repeat(incorrectLength);
 
-  it.each`
-    password           | expected
-    ${validPassword}   | ${true}
-    ${invalidPassword} | ${false}
-  `(
-    "checks that a password contains at least one digit",
-    ({ password, expected }) => {
-      const validator = new PasswordValidator();
-      expect(validator.execute(password)).toBe(expected);
-    }
-  );
+    const validator = new PasswordValidator();
+
+    expect(validator.execute(invalidPassword)).toBe(false);
+    expect(validator.execute(validPassword)).toBe(true);
+  });
+
+  it("checks that a password contains at least one digit", () => {
+    bypassIrrelevantPrivateMethodsForUseCase("hasDigit");
+    const validPassword = "1";
+    const invalidPassword = "a";
+
+    const validator = new PasswordValidator();
+
+    expect(validator.execute(invalidPassword)).toBe(false);
+    expect(validator.execute(validPassword)).toBe(true);
+  });
+
+  it("checks that a password contains at least one capital letter", () => {
+    bypassIrrelevantPrivateMethodsForUseCase("containsCapitalLetter");
+    const validPassword = "A";
+    const invalidPassword = "a";
+
+    const validator = new PasswordValidator();
+
+    expect(validator.execute(invalidPassword)).toBe(false);
+    expect(validator.execute(validPassword)).toBe(true);
+  });
 });
+
+function bypassIrrelevantPrivateMethodsForUseCase(useCaseMethod: string): void {
+  const excludedMethodsForStubs = ["constructor", "execute"];
+  const methodsForStubs = Reflect.ownKeys(PasswordValidator.prototype)
+    .filter(
+      (method) =>
+        ![useCaseMethod, ...excludedMethodsForStubs].includes(method as string)
+    )
+    .map(String);
+    
+  methodsForStubs.forEach((method) => {
+    jest
+      .spyOn(PasswordValidator.prototype as any, method)
+      .mockImplementation(() => true);
+  });
+}
