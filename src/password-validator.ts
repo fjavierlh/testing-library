ce7272f248a36f74b21bf20f29218c37ab49c859
Javier López <fjavierlh@gmail.com>
2024-03-01 07:02:03 +0100

    feat: implememnt at least one digit use case

--- src/password-validator.ts
+++ src/password-validator.ts
@@ -1,6 +1,7 @@
 export class PasswordValidator {
   public execute(password: string): boolean {
     const minimumPasswordLength = 6;
-    return password.length >= minimumPasswordLength;
+    const containsDigit = password.match(/\d/);
+    return containsDigit && password.length >= minimumPasswordLength;
   }
 }
