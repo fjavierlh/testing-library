8a121a56ea26405b2d9fea7f578fe3af195ad3bf
Javier López <fjavierlh@gmail.com>
2024-03-07 07:28:52 +0100

    test: create correct lines case

--- tests/csv-filter.test.ts
+++ tests/csv-filter.test.ts
@@ -46,3 +46,15 @@ Num _factura, Fecha,      Bruto, Neto, IVA, IGIC, Concepto,    CIF_cliente, NIF_
 7. Un fichero que contiene una sola línea es incorrecto si no tiene cabecera
 
  */
+describe("The CSV filter", () => {
+  it("allows correct lines", () => {
+    const header =
+      "Num _factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente";
+    const invoiceLine = "1,02/05/2021,1000,790,21,,ACER Laptop,B76430134,";
+
+    const csvFilter = new CSVFilter([header, invoiceLine]);
+    const result = csvFilter.filteredLines;
+
+    expect(result).toBe([header, invoiceLine]);
+  });
+});
