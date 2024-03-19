ce6702abc653ce7d9fac260816a08ec1b7052bbe
Javier López <fjavierlh@gmail.com>
2024-03-19 07:20:41 +0100

    feat: implement yields value one for number one

--- src/fibonacci.ts
+++ src/fibonacci.ts
@@ -1,3 +1,4 @@
 export function fibonacci(number: number) {
+  if (number === 1) return 1;
   return 0;
 }
--- tests/fibonacci.test.ts
+++ tests/fibonacci.test.ts
@@ -1,7 +1,7 @@
 import { fibonacci } from "./../src/fibonacci";
 
 /**
- Fibbonacci Test
+ Fibonacci Test
  Example: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
  */
 
