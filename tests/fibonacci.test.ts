603329654b631039d0d693c93d25d15eaffd491c
Javier López <fjavierlh@gmail.com>
2024-03-19 07:42:15 +0100

    style: remove comment in test

--- tests/fibonacci.test.ts
+++ tests/fibonacci.test.ts
@@ -1,10 +1,5 @@
 import { fibonacci } from "./../src/fibonacci";
 
-/**
- Fibonacci Test
- Example: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
- */
-
 describe("The fibonacci sequence", () => {
   it("yields value zero for number zero", () => {
     const result = fibonacci(0);
