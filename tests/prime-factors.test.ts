759c3dd3ab237d4404788334919ac144b4068696
Javier López <fjavierlh@gmail.com>
2024-03-23 17:27:12 +0100

    style: remove comment in test

--- tests/prime-factors.test.ts
+++ tests/prime-factors.test.ts
@@ -1,15 +1,3 @@
-/*
-Cases
-2 ⇒ [2]
-2 * 2 ⇒ [2,2]
-2 * 2 * 2 ⇒ [2,2,2]
-3 ⇒ [3]
-3 * 3 ⇒ [3,3]
-3 * 2 ⇒ [2,3]
-5 * 5 ⇒ [5,5]
-5 * 7 * 11 * 3 ⇒ [3,5,7,11]
- */
-
 import { primeFactorsFor } from "../src/prime-factors";
 
 describe("The factor primes", () => {
