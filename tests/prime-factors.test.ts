1276fc64a0ef1adce45420aa24cdc6b5dc8ceeb8
Javier López <fjavierlh@gmail.com>
2024-03-23 16:48:34 +0100

    test: the prime composition of a given number

---
+++ tests/prime-factors.test.ts
@@ -0,0 +1,18 @@
+/*
+Cases
+2 ⇒ [2]
+2 * 2 ⇒ [2,2]
+2 * 2 * 2 ⇒ [2,2,2]
+3 ⇒ [3]
+3 * 3 ⇒ [3,3]
+3 * 2 ⇒ [2,3]
+5 * 5 ⇒ [5,5]
+5 * 7 * 11 * 3 ⇒ [3,5,7,11]
+ */
+
+describe("The factor primes", () => {
+  it("the prime composition of a given number", () => {
+    const result = primeFactorsFor(2);
+    expect(result).toBe([2]);
+  });
+});
