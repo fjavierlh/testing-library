0de8749458f2f7912008fb7065631337b8cce0da
Javier López <fjavierlh@gmail.com>
2024-03-23 17:52:25 +0100

    refactor: replace if statement by ternary operator

--- src/prime-factors.ts
+++ src/prime-factors.ts
@@ -1,15 +1,10 @@
 export function primeFactorsFor(num: number): number[] {
   let factor = 2;
-
   while (num % factor !== 0) {
     ++factor;
   }
-
   const remainder = num / factor;
-
-  if (remainder <= 1) {
-    return [factor];
-  } else {
-    return [factor].concat(primeFactorsFor(remainder));
-  }
+  return remainder <= 1
+    ? [factor]
+    : [factor].concat(primeFactorsFor(remainder));
 }
