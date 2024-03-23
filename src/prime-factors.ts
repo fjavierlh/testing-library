52195e0d6112feed88efa891253b5d97068614cf
Javier López <fjavierlh@gmail.com>
2024-03-23 17:54:11 +0100

    refactor: extract method for smallest prime

--- src/prime-factors.ts
+++ src/prime-factors.ts
@@ -1,10 +1,13 @@
 export function primeFactorsFor(num: number): number[] {
+  let prime = findTheSmallestPrimeFor(num);
+  const remainder = num / prime;
+  return remainder <= 1 ? [prime] : [prime].concat(primeFactorsFor(remainder));
+}
+
+function findTheSmallestPrimeFor(num: number) {
   let factor = 2;
   while (num % factor !== 0) {
     ++factor;
   }
-  const remainder = num / factor;
-  return remainder <= 1
-    ? [factor]
-    : [factor].concat(primeFactorsFor(remainder));
+  return factor;
 }
