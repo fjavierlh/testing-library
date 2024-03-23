654a1070d5e0aacdfd3a1fd88e8cad3996c87f98
Javier López <fjavierlh@gmail.com>
2024-03-23 17:32:31 +0100

    refactor(test): divide cases

--- tests/prime-factors.test.ts
+++ tests/prime-factors.test.ts
@@ -1,14 +1,16 @@
 import { primeFactorsFor } from "../src/prime-factors";
 
 describe("The factor primes", () => {
-  it("the prime composition of a given number", () => {
+  it("knows what is a prime number", () => {
     expect(primeFactorsFor(2)).toEqual([2]);
-    expect(primeFactorsFor(2 * 2)).toEqual([2, 2]);
-    expect(primeFactorsFor(2 * 2 * 2)).toEqual([2, 2, 2]);
     expect(primeFactorsFor(3)).toEqual([3]);
-    expect(primeFactorsFor(3 * 3)).toEqual([3, 3]);
-    expect(primeFactorsFor(2 * 3)).toEqual([2, 3]);
-    expect(primeFactorsFor(5 * 5)).toEqual([5, 5]);
+  });
+
+  it("produces the same result to multiply the numbers in the output list", () => {
+    expect(primeFactorsFor(2 * 2 * 2)).toEqual([2, 2, 2]);
+  });
+
+  it("orders the prime numbers from the smallest to the biggest", () => {
     expect(primeFactorsFor(3 * 5 * 7 * 11)).toEqual([3, 5, 7, 11]);
   });
 });
