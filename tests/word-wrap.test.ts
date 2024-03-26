91a0351dc4ad11dcf7c1a23045b3f90bb3e6069c
Javier López <fjavierlh@gmail.com>
2024-03-26 07:34:16 +0100

    test: add simplest case for word wrap

---
+++ tests/word-wrap.test.ts
@@ -0,0 +1,17 @@
+/*
+wordWrap('',5) ⇒ ''
+wordWrap('hello',5) ⇒ 'hello'
+wordWrap('longword',4) ⇒ 'long\nword'
+wordWrap('reallylongword',4) ⇒ 'real\nlylo\nngwo\nrd'
+wordWrap('abc def',4) ⇒ 'abc\ndef' 
+wordWrap('abc def ghi',4) ⇒ 'abc\ndef\nghi'
+wordWrap(' abcdf',4) ⇒ '\nabcd\nf'
+wordWrap(null,5) ⇒ ''
+wordWrap('hello',-5) ⇒ throw exception
+*/
+
+describe('The word wrap', () => { 
+  it("return same output if word does not need be wrapped", () => {
+    expect(wordWrap("", 5)).toBe((""))
+  } )
+ })
