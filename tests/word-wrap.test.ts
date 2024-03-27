e2cc7c143deb052e529534491361c35c68752362
Javier López <fjavierlh@gmail.com>
2024-03-27 07:49:37 +0100

    feat: makes lines if a text spaces if complete words overflows the max column width

--- tests/word-wrap.test.ts
+++ tests/word-wrap.test.ts
@@ -13,8 +13,16 @@ function wordWrap(text: string, columnWidth: number): string {
   if (text.length <= columnWidth) {
     return text;
   }
-  const wrappedText = text.substring(0, columnWidth).concat("\n");
-  const unwrappedText = text.substring(columnWidth);
+  let wrappedText: string;
+  let unwrappedText: string;
+  if (text.indexOf(" ") > -1 && text.indexOf(" ") < columnWidth) {
+    wrappedText = text.substring(0, text.indexOf(" ")).concat("\n");
+    unwrappedText = text.substring(text.indexOf(" ") + 1);
+  } else {
+    wrappedText = text.substring(0, columnWidth).concat("\n");
+    unwrappedText = text.substring(columnWidth);
+  }
+
   return wrappedText.concat(wordWrap(unwrappedText, columnWidth));
 }
 
@@ -34,5 +42,6 @@ describe("The word wrap", () => {
 
   it("makes two lines if a text with spaces overflows the max column width once", () => {
     expect(wordWrap("abc def", 4)).toBe("abc\ndef");
+    expect(wordWrap("abc def ghi", 4)).toBe("abc\ndef\nghi");
   });
 });
